06a2c0cf43f45dfbfdc1ad902e9a17409e423316
PAPA <PAPA@FEKETERETEK>
2021-12-11 07:47:47 +0100

    Add project files.

---
+++ ATM/Controllers/AtmController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ATM.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class AtmController : ControllerBase
+    {
+        [HttpGet]
+        [Route("ping")]
+        public string Index() => "☻☻☻ Listening ☻☻☻"; // just for fun
+
+    }
+}
---
+++ ATM/Model/Interfaces/IAtm.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace ATM.Model.Interfaces
+{
+    public interface IAtm
+    {
+        /// <summary>
+        ///     Egy ATM implementációtól elvárt működés
+        /// </summary>
+        public interface IAtm
+        {
+            /// <summary>
+            ///     Összeg feltöltése az ATM-be.
+            /// </summary>
+            /// <param name="charge">
+            ///     A feltöltendő összegek listája, ahol a címlet a kulcs és a darabszám az érték.
+            /// </param>
+            /// <returns>
+            ///     Az ATM pillanatnyi össz pénzkészlete.
+            /// </returns>
+            int Charge(IDictionary<int, int> charge);
+
+            /// <summary>
+            ///     Összeg kivétele az ATM-ből.
+            /// </summary>
+            /// <param name="money">
+            ///     A kiadandó összeg.
+            /// </param>
+            /// <returns>
+            ///     A kiadott összeg címlet-darabszám lista bontásban, ahol a címlet a kulcs, a darabszám az érték.
+            /// </returns>
+            IDictionary<int, int> Pay(int money);
+        }
+    }
+}
